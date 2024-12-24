d3725d54952db4ec4d3cfcc0ec8ae728d20cb770
JoshEager <josheagr07@gmail.com>
2024-12-23 23:13:16 -0500

    added readme

--- src/calc.cpp
+++ src/calc.cpp
@@ -1,7 +1,15 @@
 #include "calc.h"
 #include "exprtk.hpp"
+#include <string>
 #include <iostream>
 
 void calc() {
 
+    // First we have to start by getting something to calculate from the user (it may also be a command)
+    std::string input;
+    std::cin >> input;
+
+    // Next, we have to turn that into something that actually makes sense
+    // For this, we will use a parsing library (exprtk)
+    
 }
