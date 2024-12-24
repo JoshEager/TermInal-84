cb25a50e697865534bcffa874eefcd315e6d5c60
JoshEager <josheagr07@gmail.com>
2024-12-24 12:18:36 -0500

    fixed broken commands

--- src/calc.cpp
+++ src/calc.cpp
@@ -6,40 +6,43 @@
 #include <cmath>
 
 double parse(std::string expression_string);
-void checkForCommand(std::string expression_string);
+void checkForCommand(std::string expression_string, bool& shouldParse);
 
 void calc() {
+    bool shouldParse = true;
 
     // First we have to start by getting something to calculate from the user (it may also be a command)
     std::string expression_string;
     std::cin >> expression_string;
 
-    // Now we check to see if the string is a command, if it is then handle accordingly
-    checkForCommand(expression_string);
+    // Now we check to see if the string is a command, if it is then we shouldn't parse it. 
+    checkForCommand(expression_string, shouldParse);
 
     // Next, we have to get a result from expression string
     // For this, we will use a parsing library (exprtk)
-    double result = parse(expression_string);
+    if (shouldParse) {
+        double result = parse(expression_string);
 
-    // Once we get the result, we should check if it's nan
-    // after we know it isn't, then we should print. 
-    if (std::isnan(result)) {
-        std::cout << "Invalid Syntax! \n";
-    } else {
-        std::cout << result << '\n'; 
+        // Once we get the result, we should check if it's nan
+        // after we know it isn't, then we should print. 
+        if (std::isnan(result)) {
+            std::cout << "Invalid Syntax! \n";
+        } else {
+            std::cout << result << '\n'; 
+        }
     }
 
 }
 
-void checkForCommand(std::string expression_string) {
+void checkForCommand(std::string expression_string, bool& shouldParse) {
     if (expression_string == "clear") {
         clearScreen();
-        calc();
+        shouldParse = false;
     } else if (expression_string == "exit") {
         exit(0);
     } else if (expression_string == "help") {
         getHelp();
-        calc();
+        shouldParse = false;
     }
 }
 
