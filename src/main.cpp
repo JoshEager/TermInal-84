12afdf980ff78bf400c00d9a5eae428b240883fb
JoshEager <josheagr07@gmail.com>
2024-12-25 14:12:39 -0500

    changing main to use ncurses

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,45 @@
-#include "calc.h"
-#include <iostream>
+#include <ncurses.h>
+#include <string>
+
+void print(const char text[]);
+void startLoop();
+void evalInput();
 
 int main() { 
     
-    std::cout << "Welcome to TermInal-84! For help, type 'help'. \n";
+    const char initMessage[] { "Welcome to TermInal-84. For help, type 'help'.\n" };
 
-    while ( true ) {
-        calc(); 
-    }
+    initscr(); 
+    print(initMessage);
+
+    startLoop();  
 
     return 0; 
+}
+
+void print(const char text[]) {
+    printw(text);
+    refresh(); 
+}
+
+void startLoop() {
+    while ( true ) {
+        evalInput();
+    }
+}
+
+void evalInput() {
+    char rawInput[] { getInput() };
+    char output[] { parseInput(rawInput) };
+    print(output);
+}
+
+char getInput() {
+    char * input;
+    scanw(input);
+    return *input;
+}
+
+char parseInput(char input[]) {
+    
 }
